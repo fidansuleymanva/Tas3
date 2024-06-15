4cb0e212ab7297c8869ab840b6d1ae9604edbd77
fidansuleymanva <tu5gllte6@code.edu.az>
2024-06-15 21:01:05 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿//TASK3: Girişdə n natural ədədi verilir. Bu ədədin sağdan birinci rəqəmini tapmaq tələb olunur.
+using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a natural number:");
+            int number = int.Parse(Console.ReadLine());
+
+            int rightmostDigit = number % 10;
+            Console.WriteLine("The rightmost digit is: " + rightmostDigit);
+        }
+    }
+}
+
